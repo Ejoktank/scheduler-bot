30d02af1a56cc590206ec6c66c22f875c059b0d4
egor_kalmykov <egor.v.kalmykov@gmail.com>
2024-09-18 21:34:16 +0300

    slowly moving forward

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,13 @@ const DIALOG_STATES: DialogMode[] = [
     "SELECT_DAY",
     "SELECT_TIME",
     "WRITE_NOTIFICATION_MESSAGE",
+    "SELECT_CUSTOM_TIME",
 ];
 
 interface DialogState {
     mode: DialogMode,
-    time: number
+    in_time?: number,
+    when?: string
 }
 
 function main() {
@@ -22,13 +24,18 @@ function main() {
 
     const bot = new Telegraf(config.token);
 
+    bot.telegram.setMyCommands([
+        { command: 'start', description: 'Начать' },
+        { command: 'schedule', description: 'Посмотреть расписание' },
+    ])
+
     bot.start((ctx) => {
-        ctx.reply("Здарова!");
+        ctx.reply("Опять ты! Ладно, жми /schedule, чтобы начать");
     });
 
     bot.command('schedule', (ctx) => {
-        dialogState.set(ctx.from.id, { mode: DIALOG_STATES[0], time: 0} );
-        ctx.reply('На когда создать sms сообщение', {
+        dialogState.set(ctx.from.id, { mode: DIALOG_STATES[0], in_time: 0} );
+        ctx.reply('Выбери день', {
             reply_markup: {
                 inline_keyboard: [
                     [{ text: 'Сегодня', callback_data: 'schedule_today' }],
@@ -53,41 +60,116 @@ function main() {
             const payload = ctx.callbackQuery.data
 
             if (payload === 'schedule_today') {
-                ctx.reply("1", {
+                ctx.reply("Когда отправить напоминалку?", {
                     reply_markup: {
                         inline_keyboard: [
-                            [{ text: "30 секунд", callback_data: "seconds_30" }],
-                            [{ text: "15 минут", callback_data: "minutes_15" }],
-                            [{ text: "30 минут", callback_data: "minutes_30" }],
-                            [{ text: "Задать время", callback_data: "set_time" }],
+                            [{ text: "5 секунд", callback_data: "seconds_5" }],
+                            [{ text: "5 минут", callback_data: "minutes_5" }, { text: "15 минут", callback_data: "minutes_15" }],
+                            [{ text: "30 минут", callback_data: "minutes_30" }, { text: "1 час", callback_data: "hours_1" }],
+                            [{ text: "2 часа", callback_data: "hours_2" }, { text: "3 часа", callback_data: "hours_3" }],
+                            [{ text: "Днём", callback_data: "midday" }, { text: "Вечером", callback_data: "evening" }],
+                            [{ text: "Перед сном", callback_data: "night" }, { text: "Задать время", callback_data: "set_time" }],
+                        ]
+                    }
+                });                
+                dialogState.set(ctx.from.id, {
+                    mode: DIALOG_STATES[1],
+                    in_time: 0
+                });
+            }
+            if (payload === 'schedule_tomorrow' || payload === 'schedule_one_day') {
+                ctx.reply("Когда отправить напоминалку?", {
+                    reply_markup: {
+                        inline_keyboard: [
+                            [{ text: "Утром", callback_data: "morning" }],
+                            [{ text: "Днём", callback_data: "midday" }, { text: "Вечером", callback_data: "evening" }],
+                            [{ text: "Перед сном", callback_data: "night" }, { text: "Задать время", callback_data: "set_time" }],
                         ]
                     }
                 });
-                dialogState.set(ctx.from.id, {
-                    mode: DIALOG_STATES[1],
-                    time: 0
-                });
-            }
-            if (payload === 'schedule_tomorrow') {
-                ctx.reply("2");
-                dialogState.delete(ctx.from.id);
-            }
-            if (payload === 'schedule_one_day') {
-                ctx.reply("3");
                 dialogState.delete(ctx.from.id);
             }
         }
         if (mode == DIALOG_STATES[1]) {
             await ctx.answerCbQuery();
-            const payload = ctx.callbackQuery.data
+            const payload = ctx.callbackQuery.data;
+            ctx.reply("Введите сообщение");
 
-            if (payload === 'seconds_30') {
-                ctx.reply("Введите сообщение");
+            if (payload === 'seconds_5') {
                 dialogState.set(ctx.from.id, {
                     mode: DIALOG_STATES[2],
-                    time: 1000 * 5
+                    in_time: 1000 * 5
                 });
             }
+            if (payload === 'minutes_5') {
+                dialogState.set(ctx.from.id, {
+                    mode: DIALOG_STATES[2],
+                    in_time: 1000 * 60 * 5
+                });
+            }
+            if (payload === 'minutes_15') {
+                dialogState.set(ctx.from.id, {
+                    mode: DIALOG_STATES[2],
+                    in_time: 1000 * 60 * 15
+                });
+            }
+            if (payload === 'minutes_30') {
+                dialogState.set(ctx.from.id, {
+                    mode: DIALOG_STATES[2],
+                    in_time: 1000 * 60 * 30
+                });
+            }
+            if (payload === 'hours_1') {
+                dialogState.set(ctx.from.id, {
+                    mode: DIALOG_STATES[2],
+                    in_time: 1000 * 60 * 60
+                });
+            }
+            if (payload === 'hours_2') {
+                dialogState.set(ctx.from.id, {
+                    mode: DIALOG_STATES[2],
+                    in_time: 1000 * 60 * 60 * 2
+                });
+            }
+            if (payload === 'hours_3') {
+                dialogState.set(ctx.from.id, {
+                    mode: DIALOG_STATES[2],
+                    in_time: 1000 * 60 * 60 * 3
+                });
+            }
+            if (payload === 'midday') {
+                dialogState.set(ctx.from.id, {
+                    mode: DIALOG_STATES[2],
+                    when: "12:00"
+                });
+            }
+            if (payload === 'evening') {
+                dialogState.set(ctx.from.id, {
+                    mode: DIALOG_STATES[2],
+                    when: "19:00"
+                });
+            }
+            if (payload === 'night') {
+                dialogState.set(ctx.from.id, {
+                    mode: DIALOG_STATES[2],
+                    when: "23:00"
+                });
+            }
+            if (payload === 'set_time') {
+                dialogState.set(ctx.from.id, {
+                    mode: DIALOG_STATES[3],
+                });
+            }
+        }
+        if (mode === DIALOG_STATES[3]) {
+            await ctx.answerCbQuery();
+            const payload = ctx.callbackQuery.data;
+            console.log(payload);
+
+            ctx.reply("Введите время в формате чч:мм");
+            dialogState.set(ctx.from.id, {
+                mode: DIALOG_STATES[2],
+            });
         }
     });
 
@@ -101,9 +183,9 @@ function main() {
 
         if (item.mode === DIALOG_STATES[2]) {
             const message = ctx.message.text;
-            const time = item.time;
+            const time = item.in_time;
             
-            ctx.reply("Поставлено на счетчик")
+            ctx.reply("Положил напоминалку в очередь. Если хочешь загрузить меня ещё работой, валяй -- /schedule")
             setTimeout(() => {
                 ctx.reply(message);
             }, time);
